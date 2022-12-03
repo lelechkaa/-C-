84e0c82a14d3ec7ca6826439eda32dd135a2382e
lelechka_a <kucherova2011@gmail.com>
2022-12-03 13:27:30 +0300

    Задача 4

---
+++ 4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Ведите первое число");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число");
+int numberSecond = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Ведите третье число");
+int numberThird = Convert.ToInt32(Console.ReadLine()); 
+
+
+if(numberFirst > numberSecond)
+{
+    if(numberFirst > numberThird)
+{
+    Console.WriteLine($"max = " + numberFirst);
+}
+
+else
+{
+    Console.WriteLine($"max = " + numberThird);
+}
+}
+
+else if(numberSecond > numberThird)
+{
+    Console.WriteLine($"max = " + numberSecond);
+}
+
+else
+{
+    Console.WriteLine($"max = " + numberThird);
+}
