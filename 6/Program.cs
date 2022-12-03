c23aca964d6aad4fef5dcacbebec1c84a3d271cd
lelechka_a <kucherova2011@gmail.com>
2022-12-03 13:29:16 +0300

    Задача 6

---
+++ 6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ведите число");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+if(number%2==0)
+{
+    Console.WriteLine($"Число четное");
+}
+else
+{
+    Console.WriteLine($"Число не четное");
+}
