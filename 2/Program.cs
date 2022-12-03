0322d350cae65064a1772b677c0412ea3f466ce3
lelechka_a <kucherova2011@gmail.com>
2022-12-03 13:24:44 +0300

    Задача 2

---
+++ 2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ведите первое число");
+int numberfirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число");
+int numbersecond = Convert.ToInt32(Console.ReadLine()); 
+
+if(numberfirst > numbersecond)
+{
+   Console.WriteLine($"max = {numberfirst}");
+   Console.WriteLine($"min = {numbersecond}");
+}
+
+if(numbersecond > numberfirst)
+{
+    Console.WriteLine($"max = {numbersecond}");
+    Console.WriteLine($"min = {numberfirst}");
+}
