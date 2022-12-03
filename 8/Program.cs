31760ec45e48155f553698e0ae408e81e9116ee5
lelechka_a <kucherova2011@gmail.com>
2022-12-03 13:30:37 +0300

    Задача 8

---
+++ 8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ведите число");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int count = 1;
+
+while(number>0) 
+{
+    Console.WriteLine(count<=number);
+    number++;
+    if(number%2==0)
+    {
+        Console.WriteLine(number+ " ");
+    }
+}
